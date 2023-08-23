6568f0ff2a64f0babb849bd729dddb7d3e31ca39
Authns <color59abe@gmail.com>
2023-08-24 00:22:52 +0900

    add verify CI sample

---
+++ go-acl/verify/sample1.test.go
@@ -0,0 +1,12 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+package main
+
+import (
+	"go-acl/util"
+)
+
+func main() {
+	defer util.Out.Flush()
+	a, b := util.Readi(), util.Readi()
+	util.Ans(a + b)
+}
