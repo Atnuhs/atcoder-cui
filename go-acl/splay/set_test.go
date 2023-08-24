eba2ff9356e08eae05dfc0e0dda4e8ea913dde0a
Authns <color59abe@gmail.com>
2023-08-24 23:56:42 +0900

    fix valiable name

---
+++ go-acl/splay/set_test.go
@@ -0,0 +1,16 @@
+package splay
+
+import "testing"
+
+func TestSplaySet_Push(t *testing.T) {
+	tests := map[string]struct {
+		pushValues []int
+	}{
+		"single value": {
+			pushValues: []int{10},
+		},
+	}
+
+	for name, tc := range tests {
+	}
+}
